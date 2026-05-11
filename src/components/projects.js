3107c67d002c2446e3ba07a94b0154482f1b8f7b
HA24RSH <harshvardhangadagade24555@gmail.com>
2026-05-11 21:57:51 +0530

    added dynamic fetching of projects

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,58 +1,33 @@
-import GitHubIcon from '@mui/icons-material/GitHub';
-import EcoCartLogo from '../assets/projects-imgs/Eco-Cart-logo.jpg'
-import './projects.css'
+import GitHubIcon from "@mui/icons-material/GitHub";
+import EcoCartLogo from "../assets/projects-imgs/Eco-Cart-logo.jpg";
+import "./projects.css";
+import projects from "../data/projectData";
+
+const ProjectCard = ({ img, title, description, gitHub }) => {
+  return (
+    <div className="project-card">
+      <img src={img} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+      <a href={gitHub} target="_blank" rel="noopener noreferrer">
+        <GitHubIcon />
+      </a>
+    </div>
+  );
+};
 
 export const Projects = () => {
-    return (
-        <div className='pj-container'>
-            <div className='pj-text'>
-                <h3 className='text'>
-                    Stuff I've Built
-                </h3>
-                <span className='span-text'>
-                    (and actually finished)
-                </span>
-            </div>
-
-            <div className='project-container'>
-                <div className='projects'>
-                    <div className='pj-img'>
-                        <img src={EcoCartLogo} alt='Eco-Cart' className='img' />
-                    </div>
-
-                    <div className='pj-details'>
-                        <div className='pj-title'>
-                            <h2>
-                                Eco-Cart
-                            </h2>
-
-                            <div className='pj-links'>
-                                <a href='https://github.com/Atharv-28/eco-cart_frontend'
-                                    target='blank' className='pj-github'>
-                                    <GitHubIcon fontSize='medium' />
-                                </a>
-                            </div>
-                        </div>
-
-                        <div className='pj-description'>
-                            <p>
-                                Eco-Cart is an e-commerce platform focused on eco-friendly                                products. It allows users to browse and purchase sustainable
-                                items, promoting a greener lifestyle. Built with React, it
-                                features a modern design and user-friendly interface for an
-                                enjoyable shopping experience.
-                            </p>
-                        </div>
-
-                        <div className='more-about'>
-                            <button className='pj-button'>
-                                <a href='' className='pj-anchor' >
-                                    View Deatils
-                                </a>
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="projects-container">
+      {projects.map((project, index) => (
+        <ProjectCard
+          key={index}
+          img={project.img}
+          title={project.title}
+          description={project.description}
+          gitHub={project.gitHub}
+        />
+      ))}
+    </div>
+  );
+};
---
+++ src/data/projectData.js
@@ -0,0 +1,18 @@
+import { GitHub } from "@mui/icons-material";
+
+const projects = [
+    {
+        img: '../assets/projects-imgs/Eco-Cart-logo.jpg',
+        title: 'Eco-Cart',
+        description: 'Eco-Cart is an e-commerce platform focused on eco-friendly products. It allows users to browse and purchase sustainable items, promoting a greener lifestyle. Built with React, it features a modern design and user-friendly interface for an enjoyable shopping experience.',
+        gitHub: 'https://github.com/Atharv-28/eco-cart_frontend'
+
+    }, 
+    {
+        title: 'Valora',
+        description: 'Valora is a interview prep platform.',
+        github: 'https://github.com/Atharv-28/Valora'
+    }
+]
+
+export default projects;
